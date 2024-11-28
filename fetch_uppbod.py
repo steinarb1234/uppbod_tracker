6d1040cb62e2b68d3b32d0205894654f9befd660
steinargv <steinar@greidsluveitan.is>
2024-11-28 10:27:08 +0000

    Update fetch_uppbod.py

--- fetch_uppbod.py
+++ fetch_uppbod.py
@@ -78,7 +78,7 @@ try:
             df_combined = df
 
         # Sort by 'auctionDate'
-        df_combined.sort_values(by='auctionDate', inplace=True)
+        df_combined.sort_values(by='auctionDate', inplace=True, ascending=False)
 
         # Define the first few columns to order
         first_columns = ['auctionType', 'lotName', 'auctionDate', 'auctionTime', 'petitioners', 'last_fetched']
